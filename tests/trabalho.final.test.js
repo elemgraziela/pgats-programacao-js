76e4fc127dc5868bf632463b7af88df05e7c3a27
elemgraziela <elemgraziela@yahoo.com.br>
2025-06-25 08:42:13 -0300

    Atualizacao

--- tests/trabalho.final.test.js
+++ tests/trabalho.final.test.js
@@ -5,7 +5,7 @@ import {
   calcularConsumoDeRacao,
   decidirTipoDeAtividadePorPorte,
   buscarDadoAsync
-} from './trabalho.spec.js';
+} from './trabalho.js';
 
 test.describe('Testes da disciplina - fundamentos JS', () => {
   test('nome do pet em maiúsculas', () => {
